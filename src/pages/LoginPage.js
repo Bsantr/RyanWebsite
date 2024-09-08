58908c9ddd4d8d833a6ea327faf4002a6b4b935d
Ryan von May <rsa142616@stud.gibb.ch>
2024-09-09 01:33:38 +0200

    okay now for vonmay.com

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -15,7 +15,7 @@ const LoginPage = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch('https://api.ryanvonmay.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
