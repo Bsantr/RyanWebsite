dba1510ff8ff8e75be0acbd187da75cd4873cfbf
Ryan von May <rsa142616@stud.gibb.ch>
2024-09-09 02:10:41 +0200

    documents

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -15,7 +15,7 @@ const LoginPage = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://ryanvonmay.com/api/login', { // Verwende die vollständige URL deiner gehosteten API
+      const response = await fetch('https://ryanvonmay.com/documents', { // Verwende die vollständige URL deiner gehosteten API
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
